127faede6a65406856cfeec4bd7e6f394713808b
Florian Bauer <florian@fsrv.xyz>
2023-06-17 16:08:39 +0000

    squash: patch-EA1106C9-ED92-4F5D-BBA5-E3CED32738EE
    Squashed commit of the following:
    
    * ci: bigger testfile
    
    Signed-off-by: Florian Bauer <florian@fsrv.xyz>
    
    * ci: move to test system for integration test
    
    Signed-off-by: Florian Bauer <florian@fsrv.xyz>
    
    * ci: works on my machine
    
    Signed-off-by: Florian Bauer <florian@fsrv.xyz>
    
    * ci: chop filename
    
    Signed-off-by: Florian Bauer <florian@fsrv.xyz>
    
    * ci: use correct hash algorithm
    
    Signed-off-by: Florian Bauer <florian@fsrv.xyz>
    
    * ci: use correct url
    
    Signed-off-by: Florian Bauer <florian@fsrv.xyz>
    
    * fix: some advanced test
    
    Signed-off-by: Florian Bauer <florian@fsrv.xyz>
    
    * ci: test upload and download
    
    Signed-off-by: Florian Bauer <florian@fsrv.xyz>
    
    * ci: debugging
    
    Signed-off-by: Florian Bauer <florian@fsrv.xyz>
    
    See merge request https://ref.ci/fsrvcorp/services/transfer/-/merge_requests/27

--- .it/test.sh
+++ .it/test.sh
@@ -1,4 +1,26 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-env
+export DEBIAN_FRONTEND=noninteractive
+
+URL="https://${DEFAULT_INGRESS_URL}"
+apt update -qq && apt install -y -qq curl >/dev/null
+dd if=/dev/urandom of=./testfile.bin bs=1M count=1000
+
+checksum="$(sha512sum ./testfile.bin | awk '{print $1}')"
+
+curl --upload-file ./testfile.bin "$URL" | tee ./testfile.url
+curl "$(cat ./testfile.url)" -o ./testfile.downloaded
+
+transfer_checksum="$(curl "$(cat ./testfile.url)/sum" | awk '{print $1}')"
+if [ "$checksum" != "$transfer_checksum" ]; then
+    echo "Checksums do not match!"
+    exit 1
+fi
+
+downloaded_checksum="$(sha512sum ./testfile.downloaded | awk '{print $1}')"
+if [ "$checksum" != "$downloaded_checksum" ]; then
+    echo "Checksums do not match!"
+    exit 1
+fi
+
